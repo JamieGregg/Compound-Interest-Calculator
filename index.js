ef517533429cc4583f3e514315d24a7c88c1b2fe
Jamie Gregg <jamiegregg25@gmail.com>
2019-08-29 10:31:26 +0100

    Navbar is responsive to mobile devices

--- index.js
+++ index.js
@@ -17,6 +17,11 @@ var yearlyInterest;
 var totalInterest;
 var investmentValue;
 
+//navigation bar
+$(document).ready(function() {
+  $('.sidenav').sidenav();
+});
+
 //assigning all the values to the variables
 $(document).ready(function() {
   $(".submit-investment").click(function() {
@@ -33,33 +38,32 @@ $(document).ready(function() {
 });
 
 //Checking to see if the inflation checkbox is checked
-$(document).ready(function(){
-  $(".inflation-check").change(function(){
-    if(this.checked){
+$(document).ready(function() {
+  $(".inflation-check").change(function() {
+    if (this.checked) {
       $(".inflation-rate").fadeIn("fast");
-    }
-    else{
+    } else {
       $(".inflation-rate").fadeOut("fast");
     }
   });
 });
 
-function interestCompound(){
+function interestCompound() {
   var a;
   var a1;
   var p = monthlyDeposit;
   var r = inflationRate;
   var n = 12;
   var t = yearsInvested;
-  var power = n*t;
+  var power = n * t;
 
-  a1 = (1 + r/n);
-  a = p * Math.pow(a1,power);
+  a1 = (1 + r / n);
+  a = p * Math.pow(a1, power);
 
   return a;
 }
 
-function compoundInterestCalculation(){
+function compoundInterestCalculation() {
   //Forumla Variables
   var compoundSubCalc = 0;
   var principalInterest = 0;
@@ -69,7 +73,7 @@ function compoundInterestCalculation(){
 
   //Declarations of input
   var principal = initalInvestment;
-  var interestRate = annualGrowth/100;
+  var interestRate = annualGrowth / 100;
   var monthlyContibution = monthlyDeposit;
   var months = 12;
   var time = 1;
@@ -81,37 +85,37 @@ function compoundInterestCalculation(){
   totalInterest = 0;
 
   //Calculation of the compound interest using -> Total = [ P(1+r/n)^(nt) ] + [ PMT × (((1 + r/n)^(nt) - 1) / (r/n))
-  for(var i = 1; i <= yearsInvested; i++){
+  for (var i = 1; i <= yearsInvested; i++) {
 
     //ths works out inflation increase on deposits
-    if(inflation == true && i > 1){
+    if (inflation == true && i > 1) {
       var ans;
       var tempAns;
       var p = monthlyContibution;
-      var r = inflationRate/100;
+      var r = inflationRate / 100;
       var n = 12;
       var t = 1;
-      var power = n*t;
+      var power = n * t;
 
-      tempAns = (1 + r/n);
-      ans = p * Math.pow(tempAns,power);
+      tempAns = (1 + r / n);
+      ans = p * Math.pow(tempAns, power);
 
       monthlyContibution = ans;
     }
 
-    compoundSubCalc = (parseFloat(1) + parseFloat(interestRate/months));
+    compoundSubCalc = (parseFloat(1) + parseFloat(interestRate / months));
     principalInterest = principal * Math.pow(compoundSubCalc, powerCalc);
-    futureValueSubCalc = parseFloat(1 + interestRate/ months);
-    futureValueSeries = monthlyContibution * (Math.pow(futureValueSubCalc, powerCalc) -1) / (parseFloat(interestRate/months));
+    futureValueSubCalc = parseFloat(1 + interestRate / months);
+    futureValueSeries = monthlyContibution * (Math.pow(futureValueSubCalc, powerCalc) - 1) / (parseFloat(interestRate / months));
     total = principalInterest + futureValueSeries;
-    total = (Math.round( total * 100 )/100 ).toString();
+    total = (Math.round(total * 100) / 100).toString();
 
     //Assigning values into the table
-    monthlyDeposit = monthlyContibution*12;
+    monthlyDeposit = monthlyContibution * 12;
     parseFloat(monthlyDeposit);
-    totalDeposit = parseFloat(totalDeposit) + parseFloat(monthlyContibution*12);
+    totalDeposit = parseFloat(totalDeposit) + parseFloat(monthlyContibution * 12);
     parseFloat(totalDeposit);
-    yearlyInterest = (total - principal) - (monthlyContibution*12);
+    yearlyInterest = (total - principal) - (monthlyContibution * 12);
     parseFloat(yearlyInterest);
     totalInterest = total - totalDeposit;
     parseFloat(totalInterest);
@@ -119,6 +123,6 @@ function compoundInterestCalculation(){
 
     //Assigning values to the table
 
-    $(".table-investment tr:last").after("<tr><td>" + i +"</td><td>£" + monthlyDeposit.toFixed(2) + "</td><td>£" + totalDeposit.toFixed(2) + "</td><td>£" + yearlyInterest.toFixed(2) + "</td><td>£" + totalInterest.toFixed(2) + "</td><td>£" + total +"</td></tr>");
+    $(".table-investment tr:last").after("<tr><td>" + i + "</td><td>£" + monthlyDeposit.toFixed(2) + "</td><td>£" + totalDeposit.toFixed(2) + "</td><td>£" + yearlyInterest.toFixed(2) + "</td><td>£" + totalInterest.toFixed(2) + "</td><td>£" + total + "</td></tr>");
   }
 }
